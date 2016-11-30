bb381e5ce8914f8a41945fb60a49f54e0473b81f
Bur Benjamin <benjamin.bur@asinfotrack.ch>
2016-11-30 19:36:28 +0100

    #14 MainActivity
    JavaDoc

--- app/src/main/java/ch/fhnw/ip5/emotionhunt/activities/ExperienceDetailActivity.java
+++ app/src/main/java/ch/fhnw/ip5/emotionhunt/activities/ExperienceDetailActivity.java
@@ -1,15 +1,10 @@
 package ch.fhnw.ip5.emotionhunt.activities;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import ch.fhnw.ip5.emotionhunt.R;
 import ch.fhnw.ip5.emotionhunt.models.Experience;
@@ -17,7 +12,6 @@ import ch.fhnw.ip5.emotionhunt.models.ReceivedExperience;
 
 public class ExperienceDetailActivity extends AppCompatActivity {
     public static final String EXTRA_EXPERIENCE_ID = "EXTRA_EXPERIENCE_ID";
-    private Toolbar toolbar;
     private long mExperienceId;
     private Experience mExperience;
     private TextView mTxtExperienceText;
@@ -44,6 +38,9 @@ public class ExperienceDetailActivity extends AppCompatActivity {
         initView();
     }
 
+    /**
+     * Initializes the view including all view elements and its required data.
+     */
     private void initView() {
         mTxtExperienceText = (TextView) findViewById(R.id.text_experience_detail_comment);
 
