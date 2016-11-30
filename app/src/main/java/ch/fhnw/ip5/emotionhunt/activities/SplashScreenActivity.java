db180fe75572b6dd4d8380399ed1c203a42a36ae
eldoradoweb <suterdimitri@gmail.com>
2016-11-30 14:23:45 +0100

    #9 Created SplashScreen
     incl RestTask

---
+++ app/src/main/java/ch/fhnw/ip5/emotionhunt/activities/SplashScreenActivity.java
@@ -0,0 +1,76 @@
+package ch.fhnw.ip5.emotionhunt.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.fhnw.ip5.emotionhunt.R;
+import ch.fhnw.ip5.emotionhunt.helper.DeviceHelper;
+import ch.fhnw.ip5.emotionhunt.helpers.Params;
+import ch.fhnw.ip5.emotionhunt.models.LocationHistory;
+import ch.fhnw.ip5.emotionhunt.models.User;
+import ch.fhnw.ip5.emotionhunt.tasks.RestUserLoginTask;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ArrayList<User> users;
+    private View mContentView;
+    private String androidId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+        mContentView = (ImageView) findViewById(R.id.fullscreen_content);
+        mContentView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        checkUserId();
+
+    }
+
+    private void checkUserId() {
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        androidId = getAndroidId();
+        nameValuePairs.add(new BasicNameValuePair("androidId", androidId));
+        String url = Params.getApiActionUrl(getApplicationContext(), "user.login");
+        RestUserLoginTask restTask = new RestUserLoginTask(this,url,nameValuePairs);
+        restTask.execute();
+    }
+
+    public String getAndroidId() {
+        return DeviceHelper.getDeviceId(getApplicationContext());
+    }
+}
---
+++ app/src/main/java/ch/fhnw/ip5/emotionhunt/tasks/RestUserLoginTask.java
@@ -0,0 +1,65 @@
+package ch.fhnw.ip5.emotionhunt.tasks;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import com.google.gson.JsonParseException;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.IOException;
+import java.util.List;
+import ch.fhnw.ip5.emotionhunt.activities.MainActivity;
+import ch.fhnw.ip5.emotionhunt.activities.OnBoardActivity;
+
+/**
+ * EmotionHunt ch.fhnw.ip5.emotionhunt.tasks
+ *
+ * @author Benjamin Bur
+ */
+
+public class RestUserLoginTask extends RestTask {
+
+    public static final String TAG = RestUserLoginTask.class.getSimpleName();
+
+    public RestUserLoginTask(Context context, String url, List<NameValuePair> nameValuePairs)
+    {
+        super(context, url, nameValuePairs);
+    }
+
+    @Override
+    protected Boolean doInBackground(String... urls) {
+        try {
+            Log.d(TAG, "Execute: " + mUrl);
+            HttpPost httppost = new HttpPost(mUrl);
+            httppost.setEntity(new UrlEncodedFormEntity(mNameValuePairs));
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response = httpclient.execute(httppost);
+
+            int status = response.getStatusLine().getStatusCode();
+
+            Log.d(TAG, "Status: " + status);
+
+            if (status == 200) {
+                Intent intent = new Intent(mContext, MainActivity.class);
+                mContext.startActivity(intent);
+
+            } else if ( status == 403 ) {
+                Intent intent = new Intent(mContext, OnBoardActivity.class);
+                mContext.startActivity(intent);
+            } else {
+                Log.e(TAG, "Error while login. Status: " + status);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
