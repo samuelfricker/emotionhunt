625f21c6d460dd69b4d26ca98483f286ce0603a3
eldoradoweb <suterdimitri@gmail.com>
2016-11-30 16:26:58 +0100

    #8 Onboarding Activity
     Implemented Onboarding Activity incl User Registration

--- app/src/main/java/ch/fhnw/ip5/emotionhunt/activities/OnBoardActivity.java
+++ app/src/main/java/ch/fhnw/ip5/emotionhunt/activities/OnBoardActivity.java
@@ -1,44 +1,37 @@
 package ch.fhnw.ip5.emotionhunt.activities;
 
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.Loader;
-import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
-
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import java.util.ArrayList;
+import java.util.List;
 import ch.fhnw.ip5.emotionhunt.R;
+import ch.fhnw.ip5.emotionhunt.helper.DeviceHelper;
+import ch.fhnw.ip5.emotionhunt.helpers.Params;
+import ch.fhnw.ip5.emotionhunt.tasks.RestUserRegisterTask;
 
 /**
  * A login screen that offers login via name/phonenumber.
  */
-public class OnBoardActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class OnBoardActivity extends AppCompatActivity {
     private static final String TAG = OnBoardActivity.class.getSimpleName();
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
-
-    // UI references.
-    private TextView mName;
-    private TextView mPhoneNumber;
+    private EditText mName;
+    private EditText mPhoneNumber;
+    private ScrollView loginForm;
     private Button mLoginButton;
+    private String androidId;
+    public String name;
+    public String phoneNumber;
+
 
 
     @Override
@@ -46,15 +39,23 @@ public class OnBoardActivity extends AppCompatActivity implements LoaderCallback
         Log.d(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board);
-        // Set up the login form.
-        mName = (TextView) findViewById(R.id.text_login_name);
-        mPhoneNumber = (TextView) findViewById(R.id.text_login_phone_number);
+        loginForm = (ScrollView) findViewById(R.id.login_form);
+        loginForm.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
+        mName = (EditText) findViewById(R.id.text_login_name);
+        mPhoneNumber = (EditText) findViewById(R.id.text_login_phone_number);
 
         mLoginButton = (Button) findViewById(R.id.login_button);
         mLoginButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                //attemptLogin();
+                attemptRegistration();
             }
         });
 
@@ -66,23 +67,19 @@ public class OnBoardActivity extends AppCompatActivity implements LoaderCallback
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
-    private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
+    private void attemptRegistration() {
         // Reset errors.
         mName.setError(null);
         mPhoneNumber.setError(null);
 
         // Store values at the time of the login attempt.
-        String name = mName.getText().toString();
-        String phoneNumber = mPhoneNumber.getText().toString();
+        name = mName.getText().toString();
+        phoneNumber = mPhoneNumber.getText().toString();
 
         boolean cancel = false;
 
         // Check for a valid name, if the user entered one.
-        if (!TextUtils.isEmpty(name) && !isPasswordValid(name)) {
+        if (!TextUtils.isEmpty(name) && !isNameValid(name)) {
             Toast.makeText(this, R.string.empty_name, Toast.LENGTH_SHORT).show();
             cancel = true;
         }
@@ -93,79 +90,30 @@ public class OnBoardActivity extends AppCompatActivity implements LoaderCallback
             cancel = true;
         }
 
+        createNewUser();
 
-        mAuthTask = new UserLoginTask(name , phoneNumber);
-        mAuthTask.execute((Void) null);
+    }
 
+    private void createNewUser() {
+        List<NameValuePair> nameValuePairs = new ArrayList<>();
+        androidId = DeviceHelper.getDeviceId(getApplicationContext());
+        nameValuePairs.add(new BasicNameValuePair("androidId", androidId));
+        nameValuePairs.add(new BasicNameValuePair("name", name));
+        nameValuePairs.add(new BasicNameValuePair("phoneNumber", phoneNumber));
+
+        String url = Params.getApiActionUrl(getApplicationContext(), "user.register");
+        mLoginButton.setClickable(false);
+        RestUserRegisterTask restTask = new RestUserRegisterTask(this,url,nameValuePairs);
+        restTask.execute();
     }
 
     private boolean isPhoneNumberValid(String phonenumber) {
-        //TODO: Replace this with your own logic
         return phonenumber.length() > 10;
     }
 
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
+    private boolean isNameValid(String password) {
         return password.length() > 1;
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-
-    }
-
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mName;
-        private final String mPhoneNumber;
-
-        UserLoginTask(String name, String phonenumber) {
-            mName = name;
-            mPhoneNumber = phonenumber;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: Create User on API.
-
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-
-            if (success) {
-                finish();
-            } else {
-
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-
-        }
-    }
 }
 
